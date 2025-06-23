3061b5145a7a03ab3e914df6d364e9c87aa33ab5
ujjawal-cric <ujjawal.root@gmail.com>
2025-06-23 18:52:47 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -75,5 +75,7 @@ def login():
     return jsonify({"token": token})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
