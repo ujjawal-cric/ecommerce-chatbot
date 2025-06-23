4be6d459ab6b2953b259caa24e4e7e95f148233c
ujjawal-cric <ujjawal.root@gmail.com>
2025-06-23 19:15:22 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -6,7 +6,7 @@ function Login({ setToken }) {
   const [password, setPassword] = useState('');
 
   const handleLogin = async () => {
-    const res = await fetch('http://127.0.0.1:5000/login', {
+    const res = await fetch('https://ecommerce-chatbot-backend-jala.onrender.com/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password })
